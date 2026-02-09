267830a2b93172d2505d193a7c96f254b19be102
migue <miguelcoutinho181@gmail.com>
2026-02-09 08:00:46 -0300

    adicionar suporte ao Flyway para gerenciamento de migrações de banco de dados e criar tabelas iniciais para a entidade Person

---
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO erudio_db.person (address,first_name,gender,last_name) VALUES
+	 ('Uberlândia - Minas Gerais - Brasil','Leandro','Male','Costa'),
+	 ('Anchiano - Italy','Leonardo','Male','da Vinci'),
+	 ('Maricá - Rio de Janeiro - Brasil','Raquel','Female','Dos Santos Barbosa Coutinho'),
+	 ('São Paulo - São Paulo - Brasil','Miguel','Male','Coutinho Neto'),
+	 ('Maricá - Rio de Janeiro - Brasil','Rita','Female','De Fatima Magalhaes'),
+	 ('Maricá - Rio de Janeiro - Brasil','Rita','Female','De Fatima Magalhaes');
