21013f3788799382ca0ca8c184ee67470835d349
migue <miguelcoutinho181@gmail.com>
2026-02-27 00:12:16 -0300

    adicionar configuração do OpenAPI para documentação da API e atualizar PersonController com anotações Swagger

--- rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -3,6 +3,12 @@ package br.com.erudio.controllers;
 import br.com.erudio.data.dto.v1.PersonDTO;
 import br.com.erudio.data.dto.v2.PersonDTOV2;
 import br.com.erudio.services.PersonServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +16,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
     @Autowired
     private  final PersonServices service;
@@ -20,10 +27,29 @@ public class PersonController {
     }
     
     @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+    @Operation(summary = "Finds all People", description = "Finds all People", tags = {"People"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse (description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse (description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse (description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse (description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
+    @Operation(summary = "Finds a Person", description = "Finds a specific person by your id", tags = {"Person"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse (description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse (description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse (description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse (description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
     @GetMapping(value = "/{id}",
     produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
     public PersonDTO findById(@PathVariable Long id) {
